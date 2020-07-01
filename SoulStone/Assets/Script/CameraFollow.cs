6b934f6b82d90e1ace1dde152714cf78f281ef61
김승현 <52490515+pois689@users.noreply.github.com>
2020-07-01 16:47:30 +0900

    충돌체 / 카메라 스크립트 추가
    바닥 / 벽 / 박스 등 충돌체 추가
    캐릭터에 카메라 따라다니는 기능 추가

---
+++ SoulStone/Assets/Script/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform _target;
+
+    void Update()
+    {
+        //먼저 캐릭터의 위치를 가져온다.
+        Vector3 charPos = _target.position;
+        //카메라의 위치를 가져온 캐릭터의 위치로 세팅해준다. (z값은 제외)
+        float camY = transform.position.y;
+        transform.position = new Vector3(charPos.x, camY, charPos.z);
+    }
+}
