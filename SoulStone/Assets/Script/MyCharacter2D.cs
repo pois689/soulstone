9d6f2dbd122c4446ab0cec4d780513bab0758e15
김승현 <52490515+pois689@users.noreply.github.com>
2020-07-01 15:23:34 +0900

    캐릭터 에셋 추가 / 방향키 스크립트 추가
    캐릭터 에셋 추가 / 방향키 스크립트 추가 / 충돌체 추가 / 카메라 시점 및 스테이지1 로테이션 변경

---
+++ SoulStone/Assets/Script/MyCharacter2D.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyCharacter2D : MonoBehaviour
+{
+    public Rigidbody _rigid;
+    public float _jumpForce = 350.0f;
+    public float _maxForce = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //방향전환 함수
+/*    public void Flip(bool right)
+    {
+        if (right) // 오른쪽을 본다면
+        {
+            Vector3 scale = transform.localScale;
+            float newScaleX = Mathf.Abs(scale.x); // Abs = 절대값 함수, 양수가 되게 한다.
+            transform.localScale = new Vector3(newScaleX, scale.y, scale.z);
+        }
+        else // 왼쪽을 본다면
+        {
+            Vector3 scale = transform.localScale;
+            float newScaleX = Mathf.Abs(scale.x);
+            transform.localScale = new Vector3(-1 * newScaleX, scale.y, scale.z);
+        }
+
+    }*/
+}
