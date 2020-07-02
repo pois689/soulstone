787a28fe200fe7915596333a82f3854109dfa109
김승현 <52490515+pois689@users.noreply.github.com>
2020-07-02 10:59:13 +0900

    게임매니저, 클리어존, 시작화면
    게임매니저 스크립트 추가
    클리어존 스크립트 추가
    시작화면 UI 추가

--- SoulStone/Assets/Script/ClearZone.cs
+++ SoulStone/Assets/Script/ClearZone.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class ClearZone : MonoBehaviour
 {
+    public GameManager _gameMgr;
+    public int _stageNum = 0;
     // 스테이지존에 도착하면 발생하는 함수
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name=="player")
         {
-            Debug.Log("클리어존");
-            // 스테이지 클리어
-            /*UI uimgr = FindObjectOfType<UI>();
-            uimgr.Show("ui_Loading", true); // ui_Loading 띄우기
-
-            UI_Lodiong ui_Loading = FindObjectOfType<UI_Lodiong>();
-            ui_Loading.GoNext(_stageNum);*/
+            _gameMgr.GoNext(_stageNum);
         }
     }
 }
---
+++ SoulStone/Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] _startPointList;
+    public int[] _mapNum;
+    public GameObject _player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoNext(int stageNum) 
+    {
+        Debug.Log("스테이지 넘버" + stageNum);
+        GameObject startPoint = _startPointList[stageNum]; // 스테이지 넘버를 넣어준다
+
+        Vector3 pos = startPoint.transform.position;
+
+        _player.transform.position = new Vector3(pos.x, pos.y, pos.z);
+    }
+}
